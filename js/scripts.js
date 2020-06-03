d630f2f2a37e51eaa79092fab37a03c26f850e03
Tyson Lackey <lackeyt90@gmail.com>
2020-06-03 11:25:16 -0700

    add if/then block to handle multiple arithmetic logic in scripts, add multi select to form in index
    Co-Authored-by: Micheal Hansen <micheal.hansen@gmail.com>
    Co-Authored-by: Matt McFarland <mrmcfarland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -13,18 +13,30 @@ const divide = function(number1, number2) {
   };
 
 const multi = function(number1, number2) {
-  return number1 + number2;
+  return number1 * number2;
   };
   
 
 //USER INTERFACE
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#num").submit(function(event) {
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
+    const number1 = parseInt($("#num1").val());
+    const number2 = parseInt($("#num2").val());
+    const operation = $("#operation").val(); 
+    let result;
+
+    if (operation === "add") {
+      result = add(number1, number2);
+    } else if (operation === "subtract") {
+      result = sub(number1, number2);
+    } else if (operation === "divide") {
+      result = divide(number1, number2);
+    } else if (operation === "multiply") {
+      result = multi(number1, number2);
+    }
+    
     $("#output").text(result);
   });
 });
