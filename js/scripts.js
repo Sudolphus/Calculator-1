a0e7256db3ed94ddb2d9707473226f19fae7e12b
Tyson Lackey <lackeyt90@gmail.com>
2020-06-03 10:52:22 -0700

    update calc user interface, add form to index, remove alert/prompt from scripts
    Co-Authored-by: Micheal Hansen <micheal.hansen@gmail.com>
    Co-Authored-by: Matt McFarland <mrmcfarland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,31 @@
-//Addition Calculator
+//BUSINESS LOGIC:
 
 const add = function(number1, number2) {
   return number1 + number2;
   };
 
-//Subtraction Calculator
-
 const sub = function(number1, number2) {
   return number1 - number2;
   };
-
-//Division Calculator
 
 const divide = function(number1, number2) {
   return number1 / number2;
   };
 
-//Multiplication Calculator
-
 const multi = function(number1, number2) {
   return number1 + number2;
   };
   
-  const number1 = parseInt(prompt("enter a number."));
-  const number2 = parseInt(prompt("enter another number."));
 
-  const result= add(number1, number2);
-  alert(result);
+//USER INTERFACE
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
+
